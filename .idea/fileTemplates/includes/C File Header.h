c0ff4a08e4dc04a133cb76ac62f4b58bc660d4de
Noman5237 <anonyman637@gmail.com>
2019-01-14 02:12:37 +0600

    Idea Excluded

--- .idea/fileTemplates/includes/C File Header.h
+++
@@ -1,7 +0,0 @@
-#if ($HEADER_COMMENTS)
-#if ($ORGANIZATION_NAME && $ORGANIZATION_NAME != "")
-// Copyright (c) $YEAR ${ORGANIZATION_NAME}#if (!$ORGANIZATION_NAME.endsWith(".")).#end All rights reserved.
-#end
-//
-#end
-
