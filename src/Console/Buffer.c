88d55db8a599b37f737257fdc7a6b31170c199f9
Noman637 <anonyman637@gmail.com>
2019-08-21 16:44:30 +0600

    Scroll Type: Long; Modified Render and Show; Tested Scroll

--- src/Console/Buffer.c
+++ src/Console/Buffer.c
@@ -12,9 +12,7 @@ CBuffer *cbuffer_init(const char *name,
                       unsigned short int posX, unsigned short int posY,
                       size_t bufferRows, size_t bufferCols,
                       unsigned char scrollEnabled,
-                      unsigned short int noOfContainedBuffers,
-                      CBuffer **containedBuffers) {
-	
+                      unsigned short int noOfContainedBuffers, CBuffer **containedBuffers) {
 	CBuffer *cBuffer = (CBuffer *) malloc(sizeof(CBuffer));
 	
 	cBuffer->name = calloc(strlen(name) + 1, sizeof(char));
@@ -45,7 +43,7 @@ CBuffer *cbuffer_init(const char *name,
 	
 	cBuffer->scrollEnabled = (scrollEnabled == 'N' || scrollEnabled == 0) ? 0 : 1;
 	
-	cBuffer->scroll = (size_t *) calloc(2, sizeof(size_t));
+	cBuffer->scroll = (long *) calloc(2, sizeof(long));
 	cBuffer->scroll[0] = cBuffer->scroll[1] = 0;
 	
 	cBuffer->position[0] = posX;
@@ -68,8 +66,6 @@ size_t _tempStringLen = 0;
  * cBuffer
  */
 size_t _cbuffer_manageHandle(CBuffer *cBuffer, int strLength) {
-	/*	size_t i; */
-	
 	size_t len = cbuffer_maxLen(cBuffer);
 	size_t cursorPos = cBuffer->cursorPos;
 	size_t spaceAvailable = len - cursorPos;
@@ -79,19 +75,19 @@ size_t _cbuffer_manageHandle(CBuffer *cBuffer, int strLength) {
 	if (spaceAvailable >= strLength) {
 		// Appending Buffer
 		_tempCBufferPntr = cBuffer->buffer[0] + cursorPos;
-		
 		cBuffer->cursorPos += strLength;
 	} else if (strLength <= len) {
 		// Overwriting Buffer
 		
 		/* Alternative Buffer Overwriting to String Copy */
-		/*
-			for (i = 0; i < shiftLeft; i++) {
-				cBuffer->buffer[0][i] = cBuffer->buffer[0][i + shiftLeft];
-			}
-		*/
+//		/*
+		for (size_t i = 0; i < shiftLeft; i++) {
+			cBuffer->buffer[0][i] = cBuffer->buffer[0][i + shiftLeft];
+		}
+//		*/
 		
-		strcpy(cBuffer->buffer[0], cBuffer->buffer[0] + shiftLeft + 1);
+		// toKnow: strcpy produces restrict warnings
+//		strcpy(cBuffer->buffer[0], cBuffer->buffer[0] + shiftLeft + 1);
 		_tempCBufferPntr = cBuffer->buffer[0] + (len - strLength - 1);
 		
 		// At the end of overwriting and new data entry the cursor will be at the end too
@@ -126,106 +122,150 @@ void _cbuffer_render(CBuffer *cBuffer) {
 	if (cBuffer->noOfContainedBuffers) {
 		// todo: render the container buffers here
 	}
-}
-
-void cbuffer_show(CBuffer *cBuffer) {
-	_cbuffer_render(cBuffer);
-	size_t rows = (cBuffer->screenSize[0] < cBuffer->bufferSize[0]) ?
-	              cBuffer->screenSize[0] : cBuffer->bufferSize[0];
-	size_t cols = (cBuffer->screenSize[1] < cBuffer->bufferSize[1]) ?
-	              cBuffer->screenSize[1] : cBuffer->bufferSize[1];
-	
 	// fixme: Will newlines \n create an awkward situation here?
-	for (size_t i = cBuffer->scroll[0]; i < rows; i++) {
-		for (size_t j = cBuffer->scroll[1]; j < cols; j++) {
-			printf("%c", cBuffer->buffer[i][j]);
+	//fixme: Better and Cross-platform solution to clear the screen
+	system("@cls||clear");
+	
+	printf("\n");
+	for (size_t bi = cBuffer->scroll[0], si = 0; si < cBuffer->screenSize[0]; bi++, si++) {
+		for (size_t bj = cBuffer->scroll[1], sj = 0; sj < cBuffer->screenSize[1]; bj++, sj++) {
+			if ((bj < cBuffer->bufferSize[1])) {
+				printf("%c", cBuffer->buffer[bi][bj]);
+			} else{
+				printf(" ");
+			}
 		}
 		printf("\n");
 	}
-	
-	char *liveRenderCommand = calloc(100, sizeof(char));
-	printf(">>> ");
-	scanf("%[^\n]%*c", liveRenderCommand);
-	cbuffer_renderCommand(cBuffer, liveRenderCommand);
-	if (strncmp(liveRenderCommand, "quit", 4) != 0) {
-		cbuffer_show(cBuffer);
-	}
+	printf("\n");
 }
 
+char commandStr[7][15] = {
+		"focus",
+		"scroll",
+		"resize",
+		"move",
+		"remove",
+		"show",
+		"hide",
+};
+
+void (*action[7])(CBuffer *cBuffer, CBuffer *activeBuffer, const char **args) = {
+		_cbuffer_focus,
+		_cbuffer_scroll,
+		_cbuffer_resize,
+		_cbuffer_move,
+		_cbuffer_remove,
+		_cbuffer_show,
+		_cbuffer_hide,
+};
 
 void cbuffer_renderCommand(CBuffer *cBuffer, const char *command) {
-	/* toKnow: All the live render operations are done in the active buffer
-	 * toKnow: Active Buffer can be changed by focus command */
+	/*
+	 * toKnow: All the live render operations are done in the active buffer
+	 * toKnow: Active Buffer can be changed by focus action
+	 **/
 	CBuffer *activeCBuffer = cBuffer;
-	const char **commandTokens = str_split(command, " ");
+	const char **commandTokens = (const char **) str_split(command, " ");
 	
-	// todo: implement string to function pointer dictionary to render commands
+	for (int i = 0; i < 7; i++) {
+		if (!strncmp(commandTokens[1], commandStr[i], strlen(commandTokens[1]))) {
+			action[i](cBuffer, activeCBuffer, commandTokens);
+		}
+		
+	}
+	
+}
+
+void cbuffer_show(CBuffer *cBuffer) {
+	char *liveRenderCommand = calloc(100, sizeof(char));
+	do {
+		_cbuffer_render(cBuffer);
+		printf(">>> ");
+		scanf("%[^\n]%*c", liveRenderCommand);
+		if (!strncmp(liveRenderCommand, "quit", 4)) {
+			break;
+		}
+		cbuffer_renderCommand(cBuffer, liveRenderCommand);
+	} while (1);
 }
 
 /* ============================== Live Render Commands ========================= */
-void _cbuffer_scroll(CBuffer *cBuffer, const char **scrollArgs) {
+
+void _cbuffer_focus(CBuffer *cBuffer, CBuffer *activeCBuffer, const char **args) {}
+
+void _cbuffer_scroll(CBuffer *cBuffer, CBuffer *activeCBuffer, const char **args) {
 	short int noOfArgs;
 	char scrollDir;
 	long int scrollCount = 1;
-	if (scrollArgs != NULL) {
-		noOfArgs = (short int) strtol(scrollArgs[0], NULL, 10);
-		scrollDir = scrollArgs[2][0];
+	if (args != NULL) {
+		noOfArgs = (short int) strtol(args[0], NULL, 10);
+		scrollDir = args[2][0];
 		// p for pageup and q for pagedown
-		if (scrollDir == 'p' && scrollArgs[2][2] == 'd') scrollDir = 'q';
-		if (noOfArgs > 2) scrollCount = strtol(scrollArgs[3], NULL, 10);
+		if (scrollDir == 'p' && args[2][2] == 'd') scrollDir = 'q';
+		if (noOfArgs > 2) scrollCount = strtol(args[3], NULL, 10);
 		
 		switch (scrollDir) {
 			case 'u':
-				cBuffer->scroll[0] += -1 * scrollCount;
+				activeCBuffer->scroll[0] -= scrollCount;
 				break;
 			
 			case 'd':
-				cBuffer->scroll[0] += 1 * scrollCount;
+				activeCBuffer->scroll[0] += scrollCount;
 				break;
 			
 			case 'l':
-				cBuffer->scroll[1] += -1 * scrollCount;
+				activeCBuffer->scroll[1] -= scrollCount;
 				break;
 			
 			case 'r':
-				cBuffer->scroll[1] += 1 * scrollCount;
+				activeCBuffer->scroll[1] += scrollCount;
 				break;
 			
 			case 'h':
-				cBuffer->scroll[0] = cBuffer->scroll[1] = 0;
+				activeCBuffer->scroll[0] = activeCBuffer->scroll[1] = 0;
 				break;
 			
 			case 'e':
-				cBuffer->scroll[0] = cBuffer->bufferSize[0];
-				cBuffer->scroll[1] = cBuffer->bufferSize[1];
+				activeCBuffer->scroll[0] = (long) activeCBuffer->bufferSize[0] - 1;
+				activeCBuffer->scroll[1] = (long) activeCBuffer->bufferSize[1] - activeCBuffer->screenSize[1];
 				break;
 			
 			case 'p':
-				cBuffer->scroll[0] += cBuffer->screenSize[0];
+				activeCBuffer->scroll[0] -= activeCBuffer->screenSize[0];
 				break;
 			
 			case 'q':
-				cBuffer->scroll[0] -= cBuffer->screenSize[0];
+				activeCBuffer->scroll[0] += activeCBuffer->screenSize[0];
 				break;
 			
 			default:
 				break;
 		}
 	}
+	
+	// scroll can't be greater than [rows, cols] and also can't roll down to bottom past the screen
+	if (activeCBuffer->scroll[0] + activeCBuffer->screenSize[0] > activeCBuffer->bufferSize[0] - 1)
+		activeCBuffer->scroll[0] = (long) activeCBuffer->bufferSize[0] - activeCBuffer->screenSize[0];
+	if (activeCBuffer->scroll[1] + activeCBuffer->screenSize[1] > activeCBuffer->bufferSize[1] - 1)
+		activeCBuffer->scroll[1] = (long) activeCBuffer->bufferSize[1] - activeCBuffer->screenSize[1];
+	
 	// scroll can't be less than [0,0]
-	if (cBuffer->scroll[0] < 0) cBuffer->scroll[0] = 0;
-	if (cBuffer->scroll[1] < 0) cBuffer->scroll[1] = 0;
-	
-	// scroll can't be greater than [rows, cols]
-	if (cBuffer->scroll[0] > cBuffer->bufferSize[0])
-		cBuffer->scroll[0] = cBuffer->bufferSize[0];
-	if (cBuffer->scroll[1] > cBuffer->bufferSize[1])
-		cBuffer->scroll[1] = cBuffer->bufferSize[1];
-	
+	if (activeCBuffer->scroll[0] < 0) activeCBuffer->scroll[0] = 0;
+	if (activeCBuffer->scroll[1] < 0) activeCBuffer->scroll[1] = 0;
 }
+
+
+void _cbuffer_resize(CBuffer *cBuffer, CBuffer *activeCBuffer, const char **args) {}
+
+void _cbuffer_move(CBuffer *cBuffer, CBuffer *activeCBuffer, const char **args) {}
+
+void _cbuffer_remove(CBuffer *cBuffer, CBuffer *activeCBuffer, const char **args) {}
+
+void _cbuffer_show(CBuffer *cBuffer, CBuffer *activeCBuffer, const char **args) {}
+
+void _cbuffer_hide(CBuffer *cBuffer, CBuffer *activeCBuffer, const char **args) {}
 
 /* ============================== Buffer: Utility ========================= */
 
-size_t cbuffer_maxLen(CBuffer *cBuffer) {
-	return cBuffer->bufferSize[0] * cBuffer->bufferSize[1];
-}
+size_t cbuffer_maxLen(CBuffer *cBuffer) { return cBuffer->bufferSize[0] * cBuffer->bufferSize[1]; }
