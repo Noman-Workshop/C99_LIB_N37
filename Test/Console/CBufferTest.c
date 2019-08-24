97ff393523533e25d549d32272f3ef75704395c0
Noman637 <anonyman637@gmail.com>
2019-08-25 00:09:37 +0600

    Buffer Scroll Testing and Complementary String Tests

--- Test/Console/CBufferTest.c
+++ Test/Console/CBufferTest.c
@@ -6,16 +6,19 @@
 #include <Console/Buffer.h>
 
 int main() {
-	CBuffer *cBuffer = cbuffer_init("main",5, 5, 0, 0, 4, 3, 1, 0, NULL);
-	cbuffer_write(cBuffer, (cbuffer_handle, "%d23456789abc", 1));
-	printf("%s\n", cBuffer->buffer[0]);
+	CBuffer *cBuffer = cbuffer_init("main", 2, 2, 0, 0, 4, 3, 1, 0, NULL);
+	cbuffer_write(cBuffer, (cbuffer_handle, "%d2345", 1));
+	cbuffer_write(cBuffer, (cbuffer_handle, "%d789abcdef", 6));
+	cbuffer_write(cBuffer, (cbuffer_handle, "jkl"));
+	
+	cbuffer_write(cBuffer, (cbuffer_handle, "tuvwxyz0-=//llp"));
+//	cbuffer_write_exp(cBuffer, "%d23456789abc", 1);
 //	cbuffer_renderCommand(cBuffer, "scroll down 1");
 //	cbuffer_renderCommand(cBuffer, "scroll down 1");
 //	cbuffer_write(cBuffer, (cbuffer_handle, "789ABC"));
-//	printf("%s, %llu\n", cBuffer->buffer[0], strlen(cBuffer->buffer[0]));
 //	cbuffer_write(cBuffer, (cbuffer_handle, "1234567891234567"));
-//	printf("%s, %llu\n", cBuffer->buffer[0], strlen(cBuffer->buffer[0]));
-	
+	printf("%s, %d\n", cBuffer->buffer[0], (int) strlen(cBuffer->buffer[0]));
+	cbuffer_renderCommand(cBuffer, "scroll up 1000");
 	cbuffer_show(cBuffer);
 	return 0;
 }
--- Test/String/StringTest.c
+++ Test/String/StringTest.c
@@ -3,26 +3,26 @@
 # Created On: 4/9/2019; 10:54 AM
 */
 
-#include <Util/string.h>
+#include <Util/String/string.h>
 
 int main() {
-
-//	const char *hello = str_toggleCase("Hello, Hi I am No One, This is Me");
-//	printf("Toggled: %s\n", hello);
-//	char **tokens = str_split(hello, " ");
 	
-	const char *word = "bbba55bbbbbbbbbbbaa55bbbbbbbb";
-	char **tokens = str_split(word, "bbb");
-	printf("word: %s", word);
+	const char *hello = str_toggleCase("Hello, Hi I am No One, This is Me");
+//	printf("Toggled: %s\n", hello);
+	char **tokens = str_split(hello, " ");
+//
+//	const char *word = "bbba5bbbb";
+//	char **tokens = str_split(word, "bbb");
+//	printf("word: %s\n", word);
 	for (int i = 0; i <= **tokens - '0'; i++) {
-		printf("%s\n", tokens[i]);
+		printf("\"%s\" --> %d\n", tokens[i], (int) strlen(tokens[i]));
 	}
-
-//	char *s = str_trim("aaaa   5555 55 55  aaaa", "aa");
-//	printf("\"%s\", %llu", s, strlen(s));
-
-//	free((void *) hello);
-//	free(tokens);
+	
+	char *s = str_trim("aaaa55cvaabbaaaaa", "aa");
+	printf("\"%s\", %d", s, (int) strlen(s));
+	
+	free(s);
+	free(tokens);
 	return 0;
 }
 
